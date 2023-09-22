6861ab8fd9e4e5f7e8563bad24b5123963f06373
Igor Nogueira <igorcmnog@gmail.com>
2023-09-21 22:56:09 -0300

    Revert "setting api to prod"
    This reverts commit db9415df5295eec81c5f38d762d5f58c05395f67.

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
-// import { fastify } from 'fastify';
-// import cors from '@fastify/cors';
-// import { getAllPromptsRoute } from './routes/get-all-prompt';
-// import { uploadVideoRoute } from './routes/upload-video';
-// import { createTranscriptionRoute } from './routes/create-transcription';
-// import { generateAICompletionRoute } from './routes/generate-ai-completion';
+import { fastify } from 'fastify';
+import cors from '@fastify/cors';
+import { getAllPromptsRoute } from './routes/get-all-prompt';
+import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAICompletionRoute } from './routes/generate-ai-completion';
 
-// const app = fastify();
+const app = fastify();
 
-// app.register(getAllPromptsRoute);
-// app.register(uploadVideoRoute);
-// app.register(createTranscriptionRoute);
-// app.register(generateAICompletionRoute);
-// app.register(cors, {
-//   origin: '*',
-// });
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
+app.register(cors, {
+  origin: '*',
+});
 
-// app
-//   .listen({
-//     port: 3333,
-//   })
-//   .then((address) => {
-//     console.log(`Server is listening on ${address}`);
-//   });
+app
+  .listen({
+    port: 3333,
+  })
+  .then((address) => {
+    console.log(`Server is listening on ${address}`);
+  });
